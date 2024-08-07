eb23f73802a53e092463e32811e637e37e243b11
Andile <andilelethumusa@gmail.com>
2024-08-07 23:01:36 +0200

    Add Task Activity completed

--- ToDo/app/src/main/java/com/example/todo/AddActiviy.java
+++ ToDo/app/src/main/java/com/example/todo/AddActiviy.java
@@ -1,6 +1,11 @@
 package com.example.todo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddActiviy extends AppCompatActivity {
 
+    String[] priority;
+    AutoCompleteTextView list;
+    ArrayAdapter<String> priorityAdapter;
+    String item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.add_activiy);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //populate the array with the items from the string resources
+        priority = getResources().getStringArray(R.array.priority);
+
+        //Bind the dropdown with the current layout
+        priorityAdapter = new ArrayAdapter<>(this,R.layout.priority_dropdown,priority);
+        list = findViewById(R.id.list);
+        list.setAdapter(priorityAdapter);
+
+
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                item = adapterView.getItemAtPosition(i).toString();
+               // Toast.makeText(AddActiviy.this, ""+item, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
