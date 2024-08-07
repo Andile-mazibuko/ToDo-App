fa5f4b5b035e59fc69a77bea09df85dfacc95824
Andile <andilelethumusa@gmail.com>
2024-08-07 19:49:28 +0200

    Project started

---
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    public void openAddItemActivity(View v)
+    {
+        Intent intent;
+    }
+
+}
